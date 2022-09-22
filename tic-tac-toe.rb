12f368f38e07ad606bd00d48f2ab84753a75ea49
AntalThomas <Blake-Heward@hotmail.com>
2022-09-22 15:30:49 +0930

    Game works and exits when a winner is awarded

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,6 +1,3 @@
-board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-## REMEMBER TO - 1 FROM USER INPUT FOR CORRECT INDEXING
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -11,12 +8,10 @@ end
 
 def update_board(board, user_name, user_input)
   board[user_input - 1] = user_name
-
-  puts "Player #{user_name} chose board number #{user_input}"
-  display_board(board)
 end
 
-def winning_line(board, user_name)
+def check_winning_line(board, user_name)
+  gameOver = false
   winning_combo = [
     [board[0], board[1], board[2]],
     [board[3], board[4], board[5]],
@@ -29,21 +24,58 @@ def winning_line(board, user_name)
   ]
   for line in winning_combo do
     won = line.all? { |num| num == user_name }
-    return won ? "Player #{user_name} has won the game!" : next
+
+    if won == true
+      puts "\n#{user_name} has won the game!\n"
+      display_board(board)
+      exit(0)
+    end
   end
 end
-  
 
-puts "Fresh Board"
-display_board(board)
+def game(board)
+  gameOver = false
+  # Gets player1 and player2 username
+  puts "What is Player Ones username:"
+  player1 = gets.chomp.upcase
+  puts "\nWhat is Player Twos username:"
+  player2 = gets.chomp.upcase
 
-puts "Updated Board"
-update_board(board, "X", 1)
+  puts "\n#{player1} and #{player2} are playing Tic-Tac-Toe today"
+  # Plays until a player has won the round or a draw
+  while gameOver == false do
+    # Displays current board configuation
+    display_board(board)
 
-puts "Updated Board"
-update_board(board, "X", 4)
+    # Gets player ones input
+    puts "What tile does #{player1} pick:"
+    player1_choice = gets.chomp.to_i
+    puts ""
 
-puts "Updated Board"
-update_board(board, "X", 7)
+    # Updates board with player ones input
+    update_board(board, player1, player1_choice)
 
-winning_line(board, "X")
+    # Checks for a winning line for player one
+    check_winning_line(board, player1)
+
+    # Displays updated board
+    puts ""
+    display_board(board)
+
+    # Gets player twos input
+    puts "What tile does #{player2} pick:"
+    player2_choice = gets.chomp.to_i
+    puts ""
+
+    # Updates and displays board with player twos input
+    update_board(board, player2, player2_choice)
+
+    # Checks for a winning line for player two
+    check_winning_line(board, player2)
+  end
+end
+
+game_board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+## REMEMBER TO - 1 FROM USER INPUT FOR CORRECT INDEXING
+
+game(game_board)
